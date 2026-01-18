4ba805ccfb583bc43a1378a5145f3df393f3b026
Ryanny <ryannymromero@gmail.com>
2026-01-18 14:13:21 +0800

    Fix the FarmDTO

--- backend/src/main/java/com/ecocycle/backend/farm/dto/FarmDto.java
+++ backend/src/main/java/com/ecocycle/backend/farm/dto/FarmDto.java
@@ -1,13 +1,29 @@
 package com.ecocycle.backend.farm.dto;
 
+import com.ecocycle.backend.farm.model.FarmType;
+import com.ecocycle.backend.farm.model.Location;
+import com.ecocycle.backend.farm.model.FarmSize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.Set;
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class FarmDto {
+    private UUID id;
+    private String name;
+    private String description;
+    private LocationDto location;
+    private FarmSizeDto size;
+    private LocalDate establishedAt;
+    private Set<FarmType> farmTypes;
+    private String address;
+    private String imageUrl;
 }
