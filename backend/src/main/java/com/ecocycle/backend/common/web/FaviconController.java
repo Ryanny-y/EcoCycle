9e75d72348c4ae922b6d40432f258b48feb01eed
Ryanny <ryannymromero@gmail.com>
2026-02-18 12:37:11 +0800

    Add Favicon Controller

---
+++ backend/src/main/java/com/ecocycle/backend/common/web/FaviconController.java
@@ -0,0 +1,15 @@
+package com.ecocycle.backend.common.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FaviconController {
+
+    @GetMapping("favicon.ico")
+    @ResponseBody
+    void favicon() {
+        // Do nothing, just return 200 OK
+    }
+}
