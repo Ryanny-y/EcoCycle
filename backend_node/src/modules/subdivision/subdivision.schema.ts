2430ce49e06e5e1faa88fa7a5f34637c73f72553
ryanny <ryannymromero@gmail.com>
2026-04-10 18:28:04 +0800

    fix(subdivision): modify schema min error message

--- backend_node/src/modules/subdivision/subdivision.schema.ts
+++ backend_node/src/modules/subdivision/subdivision.schema.ts
@@ -2,13 +2,13 @@ import z from "zod";
 
 export const subdivisionParams = {
   params: z.object({
-    area: z.coerce.number("Area must be a number.").min(1, "Area is required."),
+    area: z.coerce.number("Area must be a number.").min(1, "Area is required and must be greater than 0."),
   }),
 };
 
 export const createSubdivision = {
   body: z.object({
     name: z.string("Subdivision name is required."),
-    area: z.coerce.number("Area must be a number.").min(1, "Area is required."),
+    area: z.coerce.number("Area must be a number.").min(1, "Area is required and must be greater than 0."),
   }),
 };
